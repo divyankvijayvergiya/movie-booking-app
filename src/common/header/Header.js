ca8d7f3eea005d4a176bec1a45420d7979e54c2d
DIVYANG VIJAYVVARGIYA <divyangvijayvvargiya@192.168.0.107>
2020-12-30 00:12:37 +0530

    login modal added

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,13 +1,53 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from "../../assets/logo.svg";
-class Header extends Component{
-    render(){
-        return(
-            <div className="div-header">
-                <img src={logo} className="app-logo" alt="logo"/>
-                <Button className="login-button" variant = "contained" color = "default">Login</Button>
+import Modal from "react-modal";
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+class Header extends Component {
+    constructor(){
+        super();
+        this.state = {
+            modalIsOpen: false,
+            value: 0
+        }
+    }
+
+    openModalHandler = ()=>{
+        this.setState({modalIsOpen: true})
+    }
+
+    closeModalHandler = () => {
+        this.setState({modalIsOpen: false})
+    }
+
+    tabChangeHandler = (event, value) => {
+        this.setState({value: value})
+    }
+    render() {
+        return (
+            <div>
+                <header className="app-header">
+                    <img src={logo} className="app-logo" alt="logo" />
+                    <Button className="login-button" variant="contained" color="default" onClick={this.openModalHandler}>Login</Button>
+                </header>
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler} style={customStyles}>
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="Login"/>
+                        <Tab label="Register"/>
+                    </Tabs>
+                </Modal>
             </div>
         )
     }
